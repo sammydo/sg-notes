a530e20bde1831eaf8a4d644dfdc523ed3384503
Samuel Udoh <sudoh@spartaglobal.co>
2017-02-27 14:44:52 +0000

    added data types to javascript file

--- week03-day1/main.js
+++ week03-day1/main.js
@@ -1,8 +1,30 @@
-// 
-// 2
-// 3
-// 4
-// // A $( document ).ready() block.
-// $( document ).ready(function() {
-//     console.log( "ready!" );
-// });
+console.log('in main.js');
+var o = {};
+var s = '';
+var b = true;
+var n = 1;
+var a = [];
+console.log('Using "typeof" operator:');
+console.log(typeof o);
+console.log(typeof s);
+console.log(typeof b);
+console.log(typeof n);
+console.log(typeof a);
+
+console.log('playing with dirrent tpes:');
+var numberAsString = '9';
+var secondString = 'bangs';
+var number = 3;
+var number1 = 1;
+var number2 = 2;
+var number3 = 3;
+console.log(numberAsString + number);
+console.log(typeof(numberAsString + number));
+console.log(number1 + number2);
+console.log(numberAsString + secondString);
+
+console.log(parseInt(numberAsString)+ number1);
+console.log(parseInt(3.13121));
+console.log(parseInt('3.13121'));
+
+console.log(parseFloat('as'));
